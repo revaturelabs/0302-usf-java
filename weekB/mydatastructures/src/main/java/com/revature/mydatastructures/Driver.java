20e11cd4bd75dfe6585dc3ff5c7b6944e1c6a852
AdamDKing <adamdentonking@gmail.com>
2020-03-11 13:16:57 -0400

    add push to MyStack

--- weekB/mydatastructures/src/main/java/com/revature/mydatastructures/Driver.java
+++ weekB/mydatastructures/src/main/java/com/revature/mydatastructures/Driver.java
@@ -3,7 +3,17 @@ package com.revature.mydatastructures;
 public class Driver {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    MyStack testStack = new MyStack();
+    
+    testStack.push("hi");
+    testStack.push("there");
+    testStack.push("elements");
+    for(int i=0; i<20; i++) {
+      testStack.push(Integer.toString(i));
+    }
+    
+    System.out.println(testStack);
+    
   }
 
 }
--- weekB/mydatastructures/src/main/java/com/revature/mydatastructures/MyStack.java
+++ weekB/mydatastructures/src/main/java/com/revature/mydatastructures/MyStack.java
@@ -1,5 +1,40 @@
 package com.revature.mydatastructures;
 
+import java.util.Arrays;
+
 public class MyStack {
+  
+  String[] contents;
+  int currentIndex;
+  
+  MyStack() {
+    super();
+    //new String array of length 10 -- 10 initial capacity
+    this.contents = new String[10];
+    this.currentIndex = -1;
+  }
+  
+  void push(String element) {
+    this.currentIndex++;
+    this.contents[currentIndex] = element;
+  }
+  
+  /**
+   * Doubles the length of the contents array,
+   * preserving existing values
+   */
+  void doubleCapacity() {
+    String[] temp = this.contents;
+    this.contents = new String[temp.length*2];
+    for(int i=0; i<temp.length; i++) {
+      this.contents[i] = temp[i];
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "MyStack [contents=" + Arrays.toString(contents) + ", currentIndex=" + currentIndex
+        + "]";
+  }
 
 }
