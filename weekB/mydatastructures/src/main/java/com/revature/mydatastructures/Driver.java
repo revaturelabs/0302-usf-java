c39e40ff5ebb413624ae3f4fbcb8e87dec3d4800
AdamDKing <adamdentonking@gmail.com>
2020-03-11 09:45:09 -0400

    data structures project skeleton

---
+++ weekB/mydatastructures/src/main/java/com/revature/mydatastructures/Driver.java
@@ -0,0 +1,9 @@
+package com.revature.mydatastructures;
+
+public class Driver {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
---
+++ weekB/mydatastructures/src/main/java/com/revature/mydatastructures/MyStack.java
@@ -0,0 +1,5 @@
+package com.revature.mydatastructures;
+
+public class MyStack {
+
+}
