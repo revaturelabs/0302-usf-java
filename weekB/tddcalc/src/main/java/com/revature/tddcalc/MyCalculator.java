1569baaf8a302f01db14de1069631b7672716e32
AdamDKing <adamdentonking@gmail.com>
2020-03-13 13:56:54 -0400

    tests for add subtract multiply for BasicCalculator

---
+++ weekB/tddcalc/src/main/java/com/revature/tddcalc/MyCalculator.java
@@ -0,0 +1,35 @@
+package com.revature.tddcalc;
+
+public class MyCalculator implements BasicCalculator {
+
+  @Override
+  public int add(int x, int y) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int subtract(int x, int y) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int multiply(int x, int y) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int divide(int x, int y) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public int fibonacci(int n) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
--- weekB/tddcalc/src/test/java/com/revature/tddcalc/CalculatorTest.java
+++ weekB/tddcalc/src/test/java/com/revature/tddcalc/CalculatorTest.java
@@ -2,7 +2,63 @@ package com.revature.tddcalc;
 
 //Canonical JUnit import statement:
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CalculatorTest {
+  
+  //This is the object we'll use to run our tests
+  // private because we don't need access elsewhere
+  private static BasicCalculator basicCalc;
+  
+  @Test
+  public void addOneToTwoGetThree() {
+    int result = basicCalc.add(1, 2);
+    assertTrue(result == 3);
+  }
+  
+  @Test
+  public void addTenToZeroGetTen() {
+    int result = basicCalc.add(10, 0);
+    assertTrue(result == 10);
+  }
+  
+  @Test
+  public void subtractTenFromZeroGetNegativeTen() {
+    int result = basicCalc.subtract(0, 10);
+    assertTrue(result == -10);
+  }
+  
+  @Test
+  public void subtractFiveFromFiftyGetFortyFive() {
+    int result = basicCalc.subtract(50, 5);
+    assertTrue(result == 45);
+  }
+  
+  @Test
+  public void multiplyOneAndTenGetTen() {
+    int result = basicCalc.multiply(1, 10);
+    assertTrue(result == 10);
+  }
+  
+  @Test
+  public void multiplyThreeAndFiveGetFifteen() {
+    int result = basicCalc.multiply(3, 5);
+    assertEquals(result, 15);
+  }
+  
+  //TODO: write tests for other methods!
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
