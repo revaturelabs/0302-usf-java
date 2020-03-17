c9441b95648cfc9b45c99fa53e30befc0d3edaf8
AdamDKing <adamdentonking@gmail.com>
2020-03-17 11:11:36 -0400

    exceptions keywords

--- week1/exceptions/src/main/java/com/revature/exceptions/Driver.java
+++ week1/exceptions/src/main/java/com/revature/exceptions/Driver.java
@@ -3,11 +3,21 @@ package com.revature.exceptions;
 public class Driver {
   
   public static void main(String[] args) {
-    
+    keywordDemo();
   }
   
+  /**
+   * First we're getting comfortable with the keywords and syntax.
+   */
   public static void keywordDemo() {
+    //our keywords are "throw", then "try" "catch" "finally", then "throws"
+    //throw new RuntimeException(); //don't worry about "Runtime" just yet
     
+    //nothing down here will run while an exception gets thrown above.
+    //throw new RuntimeException("Exception with a message");
+    
+    //RuntimeException (and just Exception) are both generic.  We often want to use a more specific Exception
+    //throw new NullPointerException();
   }
 
 }
