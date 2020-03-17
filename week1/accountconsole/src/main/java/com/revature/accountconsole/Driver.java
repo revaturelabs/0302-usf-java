903ad7c172a3e5d52943060efdf5ef335cdf150a
AdamDKing <adamdentonking@gmail.com>
2020-03-17 13:36:26 -0400

    working with scanner

--- week1/accountconsole/src/main/java/com/revature/accountconsole/Driver.java
+++ week1/accountconsole/src/main/java/com/revature/accountconsole/Driver.java
@@ -1,10 +1,50 @@
 package com.revature.accountconsole;
 
+import java.util.Scanner;
+
 public class Driver {
+  
+  /**
+   * Canonical scanner for reading console input.
+   */
+  private static Scanner sc = new Scanner(System.in);
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+   // runEcho();
+    getTokens();
+  }
+  
+  /**
+   * Runs the program that echoes user input
+   */
+  public static void runEcho() {
+    System.out.println("Starting echo, STOP to stop:");
+    //int nextInt =  sc.nextInt(); //grabs a single int, leaving the newline character
+    while(true) {
+      //whatever the user types comes to the scanner after they hit enter:
+      
+      String input = sc.nextLine();
+      if(input.equals("STOP")) {
+        break; //exit the loop
+      } else {
+        System.out.println(input);
+      }
+    }
+  }
+  
+  /**
+   * Less commonly used with System.in, your Scanner splits input into "tokens"
+   */
+  public static void getTokens() {
+    System.out.println("Splitting input into tokens, STOP to stop:");
+    while(sc.hasNext()) {
+      String word = sc.next();
+      if(word.equals("STOP")) {
+        break;
+      } else {
+        System.out.println(sc.next()); //default setting is to get you a single word.
+      }
+    }
   }
 
 }
