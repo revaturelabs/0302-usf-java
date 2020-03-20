87d5fc6a548b4207e9d2291e39ead190e7aa1ceb
AdamDKing <adamdentonking@gmail.com>
2020-03-20 13:38:14 -0400

    InspectMe class

---
+++ week1/reflections/src/main/java/com/revature/reflections/InspectMe.java
@@ -0,0 +1,30 @@
+package com.revature.reflections;
+
+/**
+ * Demo class for Reflections.  We'll inspect this class at runtime.
+ * @author Revature
+ *
+ */
+public class InspectMe {
+  
+  public InspectMe() {
+    super();
+    System.out.println("Inside of InspectMe constructor");
+  }
+  
+  public int firstMethod(int i) {
+    System.out.println("Inside of one parameter method");
+    return 0;
+  }
+  
+  public int secondMethod(int i, int j) {
+    System.out.println("Inside of two parameter method");
+    return 0;
+  }
+  
+  public int thirdMethod(int i, int j, int k) {
+    System.out.println("Inside of three parameter method");
+    return 0;
+  }
+
+}
