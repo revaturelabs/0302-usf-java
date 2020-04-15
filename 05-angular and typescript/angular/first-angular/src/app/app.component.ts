f85e2be41fabc10345298fc66a0523419cff1f10
connellrobert <robert.lel.connell@gmail.com>
2020-04-15 10:39:27 -0500

    morning of angular

--- 05-angular and typescript/angular/first-angular/src/app/app.component.ts
+++ 05-angular and typescript/angular/first-angular/src/app/app.component.ts
@@ -8,3 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'first-angular';
 }
+
+
+/*
+  The Angular cli (command line interface) gives you immense powers over how
+    you create your application. We have several commands such as "ng new [project-name]"
+    which creates a new Angular project with that name. if you want to run your application
+    on the dev server, use "ng serve".
+
+  What is Angular?
+    Angular is a Typescript based, open source, front end framework.
+    We are on version 9. version 1.x.x is js, this is AngularJS.
+      2+ is ts, this is know just as Angular.
+    Angular was made, maintained, and backed by Google.
+    Angular creates SPA's (single page applications).
+      So we only have one html, head and doctype tags. this is because
+      only one main html is sitting in the browser at all times. the
+      html that you write will be done using javascript.
+
+*/
--- 05-angular and typescript/angular/first-angular/src/app/app.module.ts
+++ 05-angular and typescript/angular/first-angular/src/app/app.module.ts
@@ -2,15 +2,61 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
+  //the classes related to views that you create go here. There can
+  //  can be three types of classes that relate to views:
+  //  components, directives, and pipes
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    //we need to import other modules when we need its classes
+    //  inside our components
     BrowserModule
   ],
+  //this is where we register our services
   providers: [],
+  //this is the root component which is the main view of the app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+  Angular provides its own system of organization of code and funcitonality
+    and this is accomplished by containers called modules.
+
+  A module is a cohesive block of code with a related set of capabilities which
+    have a specific application domain or a workflow.
+
+  All Angular applications have at least one module, the root module typically
+    is defined in the file app-module.ts. Now this module ties together all
+    of our components and defines where to start to bootstrap our application.
+
+  Some applications utilize more than one module and most commonly these modules
+    define some particular features. For example, the HTTPModule defines an API
+    to use to send AJAX requests. The BrowserModule defines functionality for running
+    your application inside a webbrowser.
+
+  All modules are typescript classes annotated with the @NgModule decorator which
+    takes in an object of properties or metadata that specify how to construct the
+    module.
+
+  Bootstrapping:
+    -Bootstrapping is an essential part in Angular where the application is loaded.
+    -the bootstrap process loads main.ts which is the entry point of the app.
+    -this process also starts the dependency injection system in Angular.
+
+  IOC
+    -Inversion of control
+    -Basically it means you are letting something else take control of something
+    -Dependency injection is a subset of ioc
+
+  Dependency Injection:
+    -A core concept that predates Angular
+      The purpose of dependency injection is to simplify dependency management in software
+        components. By reducing the amount of information a component needs to know
+        about its dependencies.
+*/
